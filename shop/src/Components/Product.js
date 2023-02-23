ecd978518d54fa16dc3e654f9ca7fff478b98bdc
habibfiras <habibfiras.hadroug@esprit.tn>
2023-02-23 11:54:09 +0100

    2

--- shop/src/Components/Product.js
+++ shop/src/Components/Product.js
@@ -8,18 +8,13 @@ import Alert from 'react-bootstrap/Alert';
 /*export default*/class Product extends React.Component {
     constructor(props){
         super(props)
-        this.state ={
-          produit :this.props.produit,
-        };
+        this.state = props;
     }
-    like=(e)=> {
-      e.preventDefault();
+    like() {
       this.setState({like: this.state.produit.like + 1});
     }
 
     render() {
-
-      
       const qt = this.state.produit.quantity;
       let button;
       if (qt === 0) {
@@ -27,23 +22,19 @@ import Alert from 'react-bootstrap/Alert';
 
       } else {
         button = <Button variant="info" >buy</Button>;
-
       }
      
-  
   return (
     <Card>
         <Card.Header>{this.state.produit.category}</Card.Header>
         <Card.Body>
-        <Card.Img
-            src={require('../assets/images/'+this.state.produit.img)}
-        />
+        <Card.Img src={require('../assets/images/'+this.state.produit.img)} />
             <Card.Title>{this.state.produit.name}</Card.Title>
             <Card.Text>
                 {this.state.produit.description}
             </Card.Text>
             <Row>
-            <Col xs lg="2">
+            <Col xs lg="10">
             <Button variant="primary" onClick={this.like}>Like {this.state.produit.like}</Button>
             </Col>
               <Col xs lg="2">
